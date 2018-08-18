4b1df2db3f6a0a3620604493ca6570112763c7f2
portbre <barrhavenporters@yahoo.ca>
2018-08-18 15:37:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace LineCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int lines = GetLineCount(args[0]);
+            Console.WriteLine("Lines: " + lines.ToString());
+        }
+
+        private static int GetLineCount(string fileName)
+        {
+            BinaryReader reader = new BinaryReader(File.OpenRead(fileName));
+            int lineCount = 1;
+            
+            char lastChar = reader.ReadChar();
+            char newChar = new char();
+        
+            do
+            {
+                newChar = reader.ReadChar();
+                if (lastChar == '\r' && newChar == '\n')
+                {
+                    lineCount++;
+                }
+                lastChar = newChar;
+            } while (reader.PeekChar() != -1);
+
+            return lineCount;
+        }
+    }
+}
